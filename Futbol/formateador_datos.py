8914834b421c4c1d1a0918e66905761d36d793f4
rubitopato <connorrgb@gmail.com>
2024-04-13 18:23:54 +0200

    completo con 50%V1

--- Futbol/formateador_datos.py
+++ Futbol/formateador_datos.py
@@ -4,9 +4,9 @@ import os
 
 def leer_csv(path: str) -> pd.DataFrame:
     
-    #url='https://www.football-data.co.uk/mmz4281/2324/SP1.csv' #2223 es el año de la liga
-    #wget.download(url, path)
-    #print('Datos Descargados!')
+    url='https://www.football-data.co.uk/mmz4281/0809/SP1.csv' #2223 es el año de la liga
+    wget.download(url, path)
+    print('Datos Descargados!')
     
     dataframe_datos_partidos = pd.read_csv(path)
     return dataframe_datos_partidos
@@ -17,9 +17,9 @@ def formatear_datos(dataframe: pd.DataFrame, path: str):
     dataframe_datos_formateados.to_csv(path, index=False)
     
 if __name__ == "__main__":
-    CARPETA_DATOS_PUROS = "Datos_Puros/"
-    CARPETA_DATOS_FORMATEADOS = "Datos_Formateados/"
-    archivos = os.listdir(CARPETA_DATOS_PUROS)
-    for archivo in archivos:
-        dataframe = leer_csv(CARPETA_DATOS_PUROS+archivo)
-        formatear_datos(dataframe=dataframe, path=CARPETA_DATOS_FORMATEADOS+archivo)
+    CARPETA_DATOS_PUROS = "C:/Users/User/OneDrive/Escritorio/Cosas_TFG/Futbol/Datos_Puros/"
+    CARPETA_DATOS_FORMATEADOS = "C:/Users/User/OneDrive/Escritorio/Cosas_TFG/Futbol/Datos_Formateados/"
+    #archivos = os.listdir(CARPETA_DATOS_PUROS)
+    #for archivo in archivos:
+    dataframe = leer_csv(CARPETA_DATOS_PUROS+'SP1-0809.csv')
+    formatear_datos(dataframe=dataframe, path=CARPETA_DATOS_FORMATEADOS+'SP1-0809.csv')
