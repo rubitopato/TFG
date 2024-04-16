159c0e3d7919ad6896295444ad5142a5cc439fe7
rubitopato <connorrgb@gmail.com>
2024-04-16 12:32:18 +0200

    ClasificacionV2 para Baloncesto + datos de futbol de diferentes ligas

--- Futbol/formateador_datos.py
+++ Futbol/formateador_datos.py
@@ -2,9 +2,10 @@ import pandas as pd
 import wget
 import os
 
-def leer_csv(path: str) -> pd.DataFrame:
-    
-    url='https://www.football-data.co.uk/mmz4281/0809/SP1.csv' #2223 es el año de la liga
+def leer_csv(path: str, year: int) -> pd.DataFrame:
+    if os.path.exists(path):
+        os.remove(path)
+    url='https://www.football-data.co.uk/mmz4281/'+year+'/SC0.csv' #2223 es el año de la liga
     wget.download(url, path)
     print('Datos Descargados!')
     
@@ -14,12 +15,16 @@ def leer_csv(path: str) -> pd.DataFrame:
 def formatear_datos(dataframe: pd.DataFrame, path: str):
     columnas = ['Div','Date','HomeTeam','AwayTeam','FTHG','FTAG','HTHG','HTAG','HS','AS','HST','AST','HY','AY','HR','AR','FTR']
     dataframe_datos_formateados = dataframe[columnas]
+    if os.path.exists(path):
+        os.remove(path)
     dataframe_datos_formateados.to_csv(path, index=False)
     
 if __name__ == "__main__":
+    years = ['1011','1112','1213','1314','1415','1516','1617','1718','1819','1920','2021','2122','2223','2324',]
     CARPETA_DATOS_PUROS = "C:/Users/User/OneDrive/Escritorio/Cosas_TFG/Futbol/Datos_Puros/"
     CARPETA_DATOS_FORMATEADOS = "C:/Users/User/OneDrive/Escritorio/Cosas_TFG/Futbol/Datos_Formateados/"
     #archivos = os.listdir(CARPETA_DATOS_PUROS)
     #for archivo in archivos:
-    dataframe = leer_csv(CARPETA_DATOS_PUROS+'SP1-0809.csv')
-    formatear_datos(dataframe=dataframe, path=CARPETA_DATOS_FORMATEADOS+'SP1-0809.csv')
+    for year in years:
+        dataframe = leer_csv(CARPETA_DATOS_PUROS+'SC0-'+year+'.csv', year)
+        formatear_datos(dataframe=dataframe, path=CARPETA_DATOS_FORMATEADOS+'SC0-'+year+'.csv')
