bf72d4ef217d09431c58781907968425db705ad0
rubitopato <connorrgb@gmail.com>
2024-04-28 14:04:06 +0200

    Tenis, Basket y futbol con medidas de correlacion

--- Futbol/formateador_datos.py
+++ Futbol/formateador_datos.py
@@ -5,7 +5,7 @@ import os
 def leer_csv(path: str, year: int) -> pd.DataFrame:
     if os.path.exists(path):
         os.remove(path)
-    url='https://www.football-data.co.uk/mmz4281/'+year+'/SC0.csv' #2223 es el año de la liga
+    url='https://www.football-data.co.uk/mmz4281/'+year+'/N1.csv' #2223 es el año de la liga
     wget.download(url, path)
     print('Datos Descargados!')
     
@@ -20,11 +20,11 @@ def formatear_datos(dataframe: pd.DataFrame, path: str):
     dataframe_datos_formateados.to_csv(path, index=False)
     
 if __name__ == "__main__":
-    years = ['1011','1112','1213','1314','1415','1516','1617','1718','1819','1920','2021','2122','2223','2324',]
+    years = ['1718','1819','1920','2021','2122','2223','2324']
     CARPETA_DATOS_PUROS = "C:/Users/User/OneDrive/Escritorio/Cosas_TFG/Futbol/Datos_Puros/"
     CARPETA_DATOS_FORMATEADOS = "C:/Users/User/OneDrive/Escritorio/Cosas_TFG/Futbol/Datos_Formateados/"
     #archivos = os.listdir(CARPETA_DATOS_PUROS)
     #for archivo in archivos:
     for year in years:
-        dataframe = leer_csv(CARPETA_DATOS_PUROS+'SC0-'+year+'.csv', year)
-        formatear_datos(dataframe=dataframe, path=CARPETA_DATOS_FORMATEADOS+'SC0-'+year+'.csv')
+        dataframe = leer_csv(CARPETA_DATOS_PUROS+'N1-'+year+'.csv', year)
+        formatear_datos(dataframe=dataframe, path=CARPETA_DATOS_FORMATEADOS+'N1-'+year+'.csv')
